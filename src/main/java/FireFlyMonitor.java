82b53982b8222e4548cb77a5bdb8796a0325e3e5
Alexander ShvetsÐ <box0259@gmail.com>
2022-02-28 15:41:00 +0400

    added java FX gui

---
+++ src/main/java/FireFlyMonitor.java
@@ -0,0 +1,38 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+
+public class FireFlyMonitor extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass()
+                    .getResource("TestGui.fxml")));
+
+            primaryStage.setTitle("Переключение модема НППУ");
+            primaryStage.setScene(new Scene(root, 400, 170, Color.web("#3C3F41")));
+            primaryStage.getIcons().add(new Image("picture/favicon.png"));
+
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
